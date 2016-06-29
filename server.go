21568442f7b0456b25cc8df4bc6252f7781e4a64
fzhouse <fzhouse1985@gmail.com>
2016-06-29 12:00:53 +0800

    file collection server

---
+++ server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func fileHandler(resp http.ResponseWriter, req *http.Request) {
+	if req.Method == "PUT" {
+		vars := mux.Vars(req)
+		filename := vars["filename"]
+		f, err := os.Create(filename)
+		if err != nil {
+			log.Println(err)
+		}
+		defer f.Close()
+		_, err = io.Copy(f, req.Body)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Printf("file %s uploaded", filename)
+	}
+}
+
+var port int
+
+func init() {
+	flag.IntVar(&port, "p", 8000, "Listen Port")
+	flag.Parse()
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/file/{filename}", fileHandler)
+	log.Printf("Listening on %d", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), r))
+}
